d0ac3fb3cbafdd66c4b98142f408d8376f8934f5
thaparoyal17@gmail.com <thaparoyal17@gmail.com>
2023-05-20 00:32:19 +0545

    Function declarations for controllers

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,1 +1,17 @@
 package controllers
+
+import "github.com/gin-gonic/gin"
+
+func HashPassword(password string) string {}
+
+func VerifyPassword(userPassword string, givenPassword string) (bool, string) {}
+
+func SignUp() gin.HandlerFunc {}
+
+func Login() gin.HandlerFunc {}
+
+func ProductViewAdmin() gin.HandlerFunc {}
+
+func SearchProduct() gin.HandlerFunc {}
+
+func SearchProductByQuery() gin.HandlerFunc {}
