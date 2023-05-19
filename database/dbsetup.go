67c09b979c55fa705dbb4600fe101b3771f297f6
thaparoyal17@gmail.com <thaparoyal17@gmail.com>
2023-05-20 01:04:56 +0545

    DBSet

--- database/dbsetup.go
+++ database/dbsetup.go
@@ -1,1 +1,42 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBSet() *mongo.Client {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongod://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Println("failed to connect to mongodb :(")
+		return nil
+	}
+
+	fmt.Println("Successfully connected to mongodb")
+	return client
+}
+
+var Client *mongo.Client = DBSet()
+
+func UserData(client *mongo.Client, collectionName string) *mongo.Collection {}
+
+func ProductData(client *mongo.Client, collectionName string) *mongo.Collection {}
