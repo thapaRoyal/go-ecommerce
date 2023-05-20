f13217a4cfec984a08ca2fd35c3d638050d8e892
thaparoyal17@gmail.com <thaparoyal17@gmail.com>
2023-05-20 11:11:26 +0545

    Signup

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,12 +1,62 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/thapaRoyal/go-ecommerce/models"
+	"go.mongodb.org/mongo-driver/bson"
+)
 
 func HashPassword(password string) string {}
 
 func VerifyPassword(userPassword string, givenPassword string) (bool, string) {}
 
-func SignUp() gin.HandlerFunc {}
+func SignUp() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var user models.User
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		validationErr := Validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr})
+			return
+		}
+
+		count, err := UserCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			return
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "user already exists"})
+		}
+
+		count, err = UserCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			return
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "this phone number is alrady in use"})
+			return
+		}
+	}
+}
 
 func Login() gin.HandlerFunc {}
 
