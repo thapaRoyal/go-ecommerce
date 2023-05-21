07012687141a7dcbdb798cf7d5d6c69796f2ebdd
thaparoyal17@gmail.com <thaparoyal17@gmail.com>
2023-05-21 20:36:03 +0545

    HashPassword and VerifyPassword

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -8,14 +8,39 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/thapaRoyal/go-ecommerce/database"
 	"github.com/thapaRoyal/go-ecommerce/models"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 )
 
-func HashPassword(password string) string {}
+var UserCollection *mongo.Collection = database.UserData(database.Client, "Users")
+var productCollection *mongo.Collection = database.ProductData(database.Client, "Products")
+var Validate = validator.New()
 
-func VerifyPassword(userPassword string, givenPassword string) (bool, string) {}
+func HashPassword(password string) string {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		log.Panic(err)
+	}
+	return string(bytes)
+}
+
+func VerifyPassword(userPassword string, givenPassword string) (bool, string) {
+	err := bcrypt.CompareHashAndPassword([]byte(givenPassword), []byte(userPassword))
+	valid := true
+	msg := ""
+
+	if err != nil {
+		msg = "Login or password is incorrect"
+		valid = false
+	}
+
+	return valid, msg
+}
 
 func SignUp() gin.HandlerFunc {
 	return func(c *gin.Context) {
