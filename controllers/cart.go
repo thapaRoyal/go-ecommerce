c1e6c03993c8e4e168718c00075f69ace1bcabcc
thaparoyal17@gmail.com <thaparoyal17@gmail.com>
2023-05-22 20:01:31 +0545

    GetItemFromCart

--- controllers/cart.go
+++ controllers/cart.go
@@ -9,6 +9,8 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/thapaRoyal/go-ecommerce/database"
+	"github.com/thapaRoyal/go-ecommerce/models"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -98,7 +100,90 @@ func (app *Application) RemoveItem() gin.HandlerFunc {
 	}
 }
 
-func GetItemFromCart() gin.HandlerFunc {}
+func GetItemFromCart() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		user_id = c.Query("id")
+
+		if user_id = ""{
+			c.Header("Content-Type","application/json")
+			c.JSON(http.StatusNotFound, gin.H{"error":"invalid id"})
+			c.Abort()
+			return
+		}
+
+		usert_id, _ := primitive.ObjectIDFromHex(user_id)
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var filledcart models.User
+		UserCollection.FindOne(ctx, bson.D{primitive.E{Key: "_id", Value: usert_id}}).Decode(&filledcart)
+
+		if err != nil {
+			log.Println(err)
+			c.IndentedJSON(500, "not found")
+			return
+		}
+
+		filter_match := bson.D{
+			{
+				Key: "$match",
+				Value: bson.D{
+					primitive.E{
+						Key: "_id",
+						Value: usert_id,
+					}
+			}}
+		}
+		unwind := bson.D{
+			{
+				Key: "$unwind",
+				Value: bson.D{
+					primitive.E{
+						Key: "path",
+						Value: "$usercart",
+					}
+				},
+			}
+		}
+		grouping := bson.D{
+			{
+				Key: "$group",
+				Value: bson.D{
+					primitive.E{
+						Key: "_id",
+						Value: "$_id",
+					},
+					{
+						Key: "total",
+						Value: bson.D{
+							primitive.E{
+								Key: "$sum",
+								Value: "$usercart.price",
+							}
+						},
+					}
+				},
+			}
+		}
+	pointcursor, err :=	UserCollection.Aggregate(ctx, mongo.Pipeline{filter_match, unwind, grouping})
+	if err != nil {
+		log.Println(err)
+	}
+
+	var listing []bson.M
+	if err = pointcursor.All(ctx, &listing); err != nil {
+		log.Println(err)
+		c.AbortWithError(http.StatusInternalServerError)
+	}
+
+	for _, json := range listing{
+		c.IndentedJSON(200, json["total"])
+		c.IndentedJSON(200, filledcart.UserCart)
+	}
+	ctx.Done()
+	}
+}
 
 func (app *Application) BuyFromCart() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -156,3 +241,4 @@ func (app *Application) InstantBuy() gin.HandlerFunc {
 		c.IndentedJSON(200, "Successfully placed the order")
 	}
 }
+
